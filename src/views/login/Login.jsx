f39697e54498fe8e2c312832a7c8cc85a0c96bc4
joker <jokersyf@163.com>
2021-01-05 22:30:22 +0800

    login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,27 +1,22 @@
-import { Component } from 'react'
 import LoginForm from './LoginForm'
 import './style/login.scss'
 
-function login (LoginForm) {
-  return class PP extends Component {
-    render () {
-      return (
-        <div className="login">
-          <div className="login-con">
-            <div className="title">
-              <p>欢迎登陆</p>
-            </div>
-            <div className="form-con">
-              <LoginForm/>
-            </div>
-          </div>
-          <div className="footer-wrap">
-            <p>copyright@2019&nbsp;XX科技&nbsp;All&nbsp;Rights&nbsp;Reserved</p>
-          </div>
+function Login () {
+  return (
+    <div className="login">
+      <div className="login-con">
+        <div className="title">
+          <p>欢迎登陆</p>
         </div>
-      )
-    }
-  }
+        <div className="form-con">
+          <LoginForm/>
+        </div>
+      </div>
+      <div className="footer-wrap">
+        <p>copyright@2019&nbsp;XX科技&nbsp;All&nbsp;Rights&nbsp;Reserved</p>
+      </div>
+    </div>
+  )
 }
 
-export default login(LoginForm)
+export default Login
